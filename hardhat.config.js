f3f2c2db0f5c608e8451453b076b63283d11e6b2
Sergio <sergioaguadogarrido@gmail.com>
2025-01-21 16:52:55 +0100

    feat: deploy wip

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,34 @@
-require('@nomiclabs/hardhat-waffle')
-require('@nomiclabs/hardhat-etherscan')
-require('hardhat-deploy')
-require('solidity-coverage')
-require('hardhat-gas-reporter')
-require('hardhat-contract-sizer')
 require('dotenv').config()
+require('hardhat-gas-reporter')
+require('solidity-coverage')
+require('hardhat-deploy')
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || 'http://eth-sepolia'
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY || '0xKey'
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || 'key'
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || 'key'
+
 module.exports = {
   solidity: '0.8.8',
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      blockConfirmations: 1,
+    },
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+      chainId: 11155111,
+      blockConfirmations: 6,
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    player: {
+      default: 1,
+    },
+  },
 }
